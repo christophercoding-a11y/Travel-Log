82e8585117bc6e0b080bfea87785a0d04223af8c
christophercodinga11y <christopherjack14@gmail.com>
2025-01-14 14:09:45 -0600

    fixing error?

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header =()=> {
         <header className="header" id="header">
             <div className="container">
                 <h1 className="display-1 text-capitalize text-center">Travel log</h1>
-                <p className="header-text text-center fst-italic">"All the places i been and some places i would love to visit one day"</p>
+                <p className="header-text text-center fst-italic">"All the places I been and some places I would love to visit one day"</p>
             </div>
         </header>
     )
